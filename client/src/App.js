5becc6eb8617165e436e8090e528c6cec72d444c
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-05 13:10:33 +0600

    client redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>App js</h1>
-      </header>
-    </div>
+    <Provider store={store}>
+      <h1>app</h1>
+    </Provider>
   );
 }
 
