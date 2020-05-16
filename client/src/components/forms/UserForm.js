ab5c8aca83fb9c515fc496bf520d2d549966104e
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-16 15:53:21 +0600

    User setting

--- client/src/components/forms/UserForm.js
+++ client/src/components/forms/UserForm.js
@@ -1,32 +1,149 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+
+import { setAlert } from "../../action/alert";
+import { updateUser } from "../../action/auth";
 
 import "./Form.css";
 
-const UserForm = () => {
+const UserForm = ({ auth: { user, loading }, setAlert, updateUser }) => {
+  const [formData, setFormData] = useState({
+    firstName: !loading && user ? user.firstName : "",
+    lastName: !loading && user ? user.lastName : "",
+    email: !loading && user ? user.email : "",
+    username: !loading && user ? user.username : "",
+    password: "",
+    password2: "",
+    oldPassword: "",
+  });
+  const {
+    email,
+    firstName,
+    lastName,
+    username,
+    password,
+    password2,
+    oldPassword,
+  } = formData;
+  const onchange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onsubmite = (e) => {
+    e.preventDefault();
+    if (oldPassword !== "") {
+      if (password !== "") {
+        if (password !== password2) {
+          setAlert("Password dose not patch", "danger");
+        } else {
+          setFormData({
+            email,
+            firstName,
+            lastName,
+            username,
+            password,
+            oldPassword,
+          });
+
+          updateUser(formData);
+          setFormData({
+            email,
+            firstName,
+            lastName,
+            username,
+            password: "",
+            password2: "",
+            oldPassword: "",
+          });
+        }
+      } else {
+        setFormData({ email, firstName, lastName, username, oldPassword });
+
+        updateUser(formData);
+        setFormData({
+          email,
+          firstName,
+          lastName,
+          username,
+          password: "",
+          password2: "",
+          oldPassword: "",
+        });
+      }
+    } else {
+      setAlert("Review to change provide your current password", "danger");
+    }
+  };
+
   return (
     <div className="from-container register">
       <div className=" card">
         <h1 className="text-center">User Update</h1>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={onsubmite}>
           <div className="form-group d-flex">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              placeholder="Enter Your First Name"
+              onChange={(e) => onchange(e)}
+            />{" "}
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              placeholder="Enter Your Last Name"
+              onChange={(e) => onchange(e)}
+            />
           </div>
           <div className="form-group d-flex">
-            <input type="Email" placeholder="Email" />
-            <input type="text" placeholder="Username" />
+            <input
+              type="email"
+              value={email}
+              name="email"
+              placeholder="Enter Your Valid Email"
+              onChange={(e) => onchange(e)}
+            />
+            <input
+              type="text"
+              value={username}
+              name="username"
+              placeholder="Enter Your Username"
+              onChange={(e) => onchange(e)}
+            />
           </div>
           <div className="form-group d-flex">
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Again password" />
+            <input
+              type="password"
+              value={password}
+              placeholder="Inter your password"
+              name="password"
+              onChange={(e) => onchange(e)}
+            />{" "}
+            <input
+              type="password"
+              value={password2}
+              placeholder="Enter your confirm password"
+              name="password2"
+              onChange={(e) => onchange(e)}
+            />
           </div>
           <div className="form-group">
-            <input type="password" placeholder="Current Password" />
+            <input
+              type="password"
+              name="oldPassword"
+              required
+              value={oldPassword}
+              placeholder="For change user setting enter your current password"
+              onChange={(e) => onchange(e)}
+            />
           </div>
           <div className="form-group float-right d-flex">
-            <input type="button" value="Update" className="btn btn-save " />
-            <input type="button" value="Cancel" className="btn " />
+            <input type="submit" value="Update" className="btn btn-save " />
+            <Link to="/">
+              {" "}
+              <input type="button" value="Cancel" className="btn " />
+            </Link>
           </div>
         </form>
       </div>
@@ -34,4 +151,8 @@ const UserForm = () => {
   );
 };
 
-export default UserForm;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { setAlert, updateUser })(UserForm);
--- client/src/components/routers/Routes.js
+++ client/src/components/routers/Routes.js
@@ -3,11 +3,11 @@ import { Switch, Route } from "react-router-dom";
 
 import PrivetRouter from "./PrivetRouter";
 import Notfound from "../layouts/Notfound";
-import Settings from "../layouts/Settings";
 
 // User
-import LoginForm from "../forms/LoginForm"
-import RegisterForm from "../forms/RegisterForm"
+import LoginForm from "../forms/LoginForm";
+import RegisterForm from "../forms/RegisterForm";
+import UserFrom from "../forms/UserForm";
 
 import { connect } from "react-redux";
 import Profile from "../profile/Profile";
@@ -18,7 +18,7 @@ const Routes = ({ auth: { loading, user } }) => {
     <Fragment>
       <Switch>
         {!loading && (
-          <PrivetRouter exact path="/setting" component={Settings} />
+          <PrivetRouter exact path="/setting" component={UserFrom} />
         )}
         <Route exact path="/register" component={RegisterForm} />
         <Route exact path="/login" component={LoginForm} />
