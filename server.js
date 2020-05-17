ac78526732a12bd56d58d5fbf5b00d7eef1723b1
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-18 02:18:12 +0600

    make it better

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ routers(app);
 const connectDB = require("./config/DB");
 connectDB();
 
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // App start on port 5000
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => console.log(`Server Start on Port ${PORT}`));
