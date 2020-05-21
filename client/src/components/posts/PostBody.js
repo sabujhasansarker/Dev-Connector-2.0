e018e9c7ef5596054e6f6d47191b374a60c2becc
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-21 15:21:45 +0600

    #4

--- client/src/components/posts/PostBody.js
+++ client/src/components/posts/PostBody.js
@@ -54,7 +54,7 @@ const PostBody = ({
                 </p>
                 <p
                   onClick={(e) => {
-                    deletePic(thumbnail);
+                    thumbnail && deletePic(thumbnail);
                     deletePost(posts._id);
                     setDot(false);
                   }}
